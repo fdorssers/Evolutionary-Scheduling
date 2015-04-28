b7a48d8eeb2d3219bcd95b93bc8cc1d2eb731919
Pieter Marsman <pietermarsman@gmail.com>
2015-04-28 14:56:57 +0200

    EA setup with DEAP

---
+++ scheduling_ea.py
@@ -0,0 +1,81 @@
+import random
+
+import numpy as np
+from deap import algorithms
+from deap import base
+from deap import creator
+from deap import tools
+
+
+def fitness(individual):
+    """
+    Calculate the fitness of the schedule
+    """
+    rooms, periods = zip(*individual)
+    return sum(rooms) + sum(periods),
+
+
+def mutate(individual, indpb=0.05):
+    """
+    Mutate the schedule
+    """
+    for i in range(0, len(individual)):
+        individual[i] = (random.randint(0, ROOMS), individual[i][1] + random.randint(-2, 2))
+    return individual,
+
+
+ROOMS = 10
+PERIODS = 100
+EXAMS = 200
+INDIVIDUALS = 100
+
+# Only use one fitness function
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+# Set the type of individuals to numpy arrays
+creator.create("Individual", list, fitness=creator.FitnessMax)
+
+# Use the toolbox to initialize the individuals
+toolbox = base.Toolbox()
+# Attributes to generate random rooms and periods
+toolbox.register("attr_exam", lambda: (random.randint(0, ROOMS), random.randint(0, PERIODS)))
+# Create the individual with alternating rooms and periods
+toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_exam, n=EXAMS)
+# Create the population as a list of the individuals
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+# Use the fitness function specified in this file
+toolbox.register("evaluate", fitness)
+# Use two point cross over
+toolbox.register("mate", tools.cxTwoPoint)
+# Use the mutation operator specified in this file
+toolbox.register("mutate", mutate, indpb=0.05)
+# Use tournament selection
+toolbox.register("select", tools.selTournament, tournsize=2)
+
+
+def main():
+    random.seed(64)
+
+    # Create population with fixed size
+    pop = toolbox.population(n=300)
+
+    # Numpy equality function (operators.eq) between two arrays returns the
+    # equality element wise, which raises an exception in the if similar()
+    # check of the hall of fame. Using a different equality function like
+    # numpy.array_equal or numpy.allclose solve this issue.
+    hof = tools.HallOfFame(5, similar=np.array_equal)
+
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("avg", np.mean)
+    stats.register("std", np.std)
+    stats.register("min", np.min)
+    stats.register("max", np.max)
+
+    algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.01, ngen=40, stats=stats, halloffame=hof)
+
+    print(pop[0])
+    return pop, stats, hof
+
+
+if __name__ == "__main__":
+    main()
