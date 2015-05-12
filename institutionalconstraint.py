73503ce4104dbb050093a9afc7337b17efa3a674
Frank Dorssers <frank.dorssers@gmail.com>
2015-05-12 15:48:41 +0200

    Add enums to remaining constraints

--- institutionalconstraint.py
+++ institutionalconstraint.py
@@ -1,9 +1,23 @@
+from enum import Enum
+
+
 class InstitutionalConstraint(object):
 
-    def __init__(self, constraint="", value_single=-1, value_list=[]):
+    def __init__(self, constraint="", values=[]):
         self.constraint = constraint
-        self.value_single = value_single
-        self.value_list = value_list
+        self.values = values
 
     def __repr__(self):
-        return 'InstitutionalConstraint(Constraint: {}, value_single: {}, value_list: {})'.format(self.constraint, self.value_single, self.value_list)
+        return 'InstitutionalConstraint(Constraint: {},value_list: {})'.format(self.constraint, self.values)
+
+
+class InstitutionalEnum(Enum):
+    TWOINAROW = 1
+    TWOINADAY = 2
+    PERIODSPREAD = 3
+    NONMIXEDDURATIONS = 4
+    FRONTLOAD = 5
+
+    @classmethod
+    def fromstring(cls, str):
+        return getattr(cls, str.upper(), None)
--- roomhardconstraint.py
+++ roomhardconstraint.py
@@ -1,3 +1,6 @@
+from enum import Enum
+
+
 class RoomHardConstraint(object):
 
     def __init__(self, constraint="", first=0, second=0):
@@ -6,3 +9,10 @@ class RoomHardConstraint(object):
 
     def __repr__(self):
         return 'RoomHardConstraint(Constraint: {}, first: {})'.format(self.constraint, self.first)
+
+class RoomHardEnum(Enum):
+    ROOM_EXCLUSIVE = 1
+
+    @classmethod
+    def fromstring(cls, str):
+        return getattr(cls, str.upper(), None)
