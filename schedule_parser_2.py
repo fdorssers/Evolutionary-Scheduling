8259a377229c4f8d57c6315a7fb54984f09e1084
Frank Dorssers <frank.dorssers@gmail.com>
2015-05-19 14:41:47 +0200

    Small simplification to constraint parsing

--- schedule_parser_2.py
+++ schedule_parser_2.py
@@ -21,12 +21,11 @@ def parse_data(lines, header_start, header_end, function):
             enumerate(lines[header_start + 1:header_end])}
 
 
-def parse_constraint(lines, header_start, header_end, function, enum_function, constraint_enum):
+def parse_constraint(lines, header_start, header_end, function, constraint_enum):
     constraints = {c: [] for c in constraint_enum}
     for line in lines[header_start + 1:header_end]:
-        vals = [l.strip() for l in line.split(',')]
-        en = enum_function(vals)
-        constraints[en].append(function(vals))
+        constraint = function([l.strip() for l in line.split(',')])
+        constraints[constraint.constraint].append(constraint)
     return constraints
 
 
@@ -41,9 +40,9 @@ def parse():
         exams = parse_data(lines, indices[EXAMS_HEADER], indices[PERIOD_HEADER], lambda x: Exam(int(x[0]), [int(i) for i in x[1:]]))
         periods = parse_data(lines, indices[PERIOD_HEADER], indices[ROOM_HEADER], lambda x: Period(x[0], x[1], int(x[2]), int(x[3])))
         rooms = parse_data(lines, indices[ROOM_HEADER], indices[PERIOD_CONSTRAINTS_HEADER], lambda x: Room(int(x[0]), int(x[1])))
-        period_constraints = parse_constraint(lines, indices[PERIOD_CONSTRAINTS_HEADER], indices[ROOM_CONSTRAINTS_HEADER], lambda x: PeriodHardConstraint(PeriodHardEnum.fromstring(x[1]), int(x[0]), int(x[2])), lambda x: PeriodHardEnum.fromstring(x[1]), PeriodHardEnum)
-        room_constraints = parse_constraint(lines, indices[ROOM_CONSTRAINTS_HEADER], indices[INSTITUTIONAL_CONSTRAINTS_HEADER], lambda x: RoomHardConstraint(RoomHardEnum.fromstring(x[1]), int(x[0])), lambda x: RoomHardEnum.fromstring(x[1]), RoomHardEnum)
-        institutional_constraints = parse_constraint(lines, indices[INSTITUTIONAL_CONSTRAINTS_HEADER], len(lines), lambda x: InstitutionalConstraint(InstitutionalEnum.fromstring(x[0]), list(map(int, x[1:]))), lambda x: InstitutionalEnum.fromstring(x[0]), InstitutionalEnum)
+        period_constraints = parse_constraint(lines, indices[PERIOD_CONSTRAINTS_HEADER], indices[ROOM_CONSTRAINTS_HEADER], lambda x: PeriodHardConstraint(PeriodHardEnum.fromstring(x[1]), int(x[0]), int(x[2])), PeriodHardEnum)
+        room_constraints = parse_constraint(lines, indices[ROOM_CONSTRAINTS_HEADER], indices[INSTITUTIONAL_CONSTRAINTS_HEADER], lambda x: RoomHardConstraint(RoomHardEnum.fromstring(x[1]), int(x[0])), RoomHardEnum)
+        institutional_constraints = parse_constraint(lines, indices[INSTITUTIONAL_CONSTRAINTS_HEADER], len(lines), lambda x: InstitutionalConstraint(InstitutionalEnum.fromstring(x[0]), list(map(int, x[1:]))), InstitutionalEnum)
         return exams, periods, rooms, period_constraints, room_constraints, institutional_constraints
 
 
