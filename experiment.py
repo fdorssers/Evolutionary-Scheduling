4a2ebd0da6bb1491d04b9e55728cfe29820667e5
Pieter Marsman <pietermarsman@gmail.com>
2015-06-02 23:57:09 +0200

    Added parameters to experiment.py

--- experiment.py
+++ experiment.py
@@ -10,11 +10,12 @@ def main(individuals=50, generations=100):
     # Parse possible commandline arguments
     individuals = int(individuals)
     generations = int(generations)
-    
+
     random.seed(64)
     info = parser.parse()
 
-    ea2 = SchedulingEA(*info, name="ea_initial_" + str(individuals), individuals=individuals, generations=generations)
+    ea2 = SchedulingEA(*info, name="ea_initial_" + str(individuals), individuals=individuals, generations=generations,
+                       indpb=0.1, tournsize=3)
     ea2.start()
 
 if __name__ == "__main__":
