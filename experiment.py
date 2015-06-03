1299f3e832bcfde181575adf2c5e77192cfaf85a
Pieter Marsman <pietermarsman@gmail.com>
2015-06-03 20:18:38 +0200

    Using time format as filename

--- experiment.py
+++ experiment.py
@@ -1,6 +1,6 @@
 import random
 import sys
-import time
+import datetime
 
 from scheduling_ea import SchedulingEA
 import schedule_parser_2 as parser
@@ -9,7 +9,7 @@ import schedule_parser_2 as parser
 __author__ = 'pieter'
 
 
-def main(individuals=10, generations=10, crossover_pb=0.5, mutation_pb=0.1):
+def main(individuals=10, generations=30, crossover_pb=0.5, mutation_pb=0.1):
     # Parse possible commandline arguments
 
     def parse_list_or_number(param, type):
@@ -30,12 +30,12 @@ def main(individuals=10, generations=10, crossover_pb=0.5, mutation_pb=0.1):
         for num_generations in generations:
             for cxpb in crossover_pb:
                 for mutpb in mutation_pb:
-                    rand = round(time.time() * 1000)
-                    ea_name = "ea_initial_{}".format(rand)
+                    rand = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S_%f")
+                    ea_name = "ea_{}".format(rand)
                     print("Starting {} with {} individuals, {} generations, {} crossover probability and {} mutation "
                           "probability".format(ea_name, num_individual, num_generations, cxpb, mutpb))
-                    ea2 = SchedulingEA(*info, name=ea_name, indi=num_individual, gen=num_generations,
-                                       indpb=0.1, tournsize=3, cxpb=cxpb, mutpb=mutpb)
+                    ea2 = SchedulingEA(*info, name=ea_name, indi=num_individual, gen=num_generations, indpb=0.1,
+                                       tournsize=3, cxpb=cxpb, mutpb=mutpb)
                     ea2.start()
 
 
