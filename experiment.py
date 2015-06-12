d4c109a68cd8166ef8b2568ec2dddc1de11e9ed0
Pieter Marsman <pietermarsman@gmail.com>
2015-06-12 14:59:58 +0200

    Saving data every 1000 seconds

--- experiment.py
+++ experiment.py
@@ -1,19 +1,22 @@
-import matplotlib
-matplotlib.use("Agg")
+from queue import Queue, Empty
 import os
 import pickle
 import random
 import sys
 import datetime
+import time
 import zipfile
-from misc import plot_ea_progress, create_directory
+
 import matplotlib.pyplot as plt
 
+from misc import plot_ea_progress, create_directory
 from scheduling_ea import SchedulingEA
 import schedule_parser_2 as parser
 
 
 __author__ = 'pieter'
+
+q = Queue()
 
 
 def main(individuals=100, generations=50, crossover_pb=0.5, mutation_pb=0.2):
@@ -43,66 +46,73 @@ def main(individuals=100, generations=50, crossover_pb=0.5, mutation_pb=0.2):
                     print("Starting {} with {} individuals, {} generations, {} crossover probability and {} mutation "
                           "probability".format(ea_name, num_individual, num_generations, cxpb, mutpb))
                     ea2 = SchedulingEA(*info, name=ea_name, indi=num_individual, gen=num_generations, indpb=0.1,
-                                       tournsize=3, cxpb=cxpb, mutpb=mutpb)
+                                       tournsize=3, cxpb=cxpb, mutpb=mutpb, save_callback=lambda ea: q.put(ea))
                     ea2.start()
                     eas.append(ea2)
 
     while len(eas) > 0:
-        for i, ea in enumerate(eas):
-            if ea.done:
-                save_date(ea)
-                del eas[i]
-                break
+        try:
+            ea = q.get(False)
+            save_data(ea)
+        except Empty:
+            for i, ea in enumerate(eas):
+                if ea.done:
+                    save_data(ea, always_save=True)
+                    del eas[i]
+                    break
 
 
-def save_date(ea):
-        def temp_file(name):
-            return os.path.join(log_dir, str(random.randint(0, 10**10)) + name)
+def save_data(ea, always_save=False):
+    def temp_file(name):
+        return os.path.join(log_dir, str(random.randint(0, 10 ** 10)) + name)
 
-        def write_to_zip_and_remove(temp, path):
-            zip_file.write(temp, path)
-            os.remove(temp)
+    def write_to_zip_and_remove(temp, path):
+        zip_file.write(temp, path)
+        os.remove(temp)
 
-        def pickle_save_zip(data, folder, file):
-            temp = temp_file(file)
-            path = os.path.join(folder, file)
-            f = open(temp, 'wb')
-            pickle.dump(data, f)
-            f.close()
-            write_to_zip_and_remove(temp, path)
+    def pickle_save_zip(data, folder, file):
+        temp = temp_file(file)
+        path = os.path.join(folder, file)
+        f = open(temp, 'wb')
+        pickle.dump(data, f)
+        f.close()
+        write_to_zip_and_remove(temp, path)
 
-        def save_str(object, folder, file):
-            temp = temp_file(file)
-            path = os.path.join(folder, file)
-            f = open(temp, 'w')
-            f.write(str(object))
-            f.close()
-            write_to_zip_and_remove(temp, path)
+    def save_str(object, folder, file):
+        temp = temp_file(file)
+        path = os.path.join(folder, file)
+        f = open(temp, 'w')
+        f.write(str(object))
+        f.close()
+        write_to_zip_and_remove(temp, path)
 
-        def save_readable_population(pop, num_rooms, num_periods, folder, file):
-            path = os.path.join(folder, file)
-            temp = temp_file(file)
-            f = open(temp, 'w')
-            pop = sorted(pop, key=lambda x: sum(x.fitness.wvalues), reverse=True)
-            for i, ind in enumerate(pop):
-                fitt_comp = "(" + ") + (".join(
-                    map(lambda x: "*".join(map(str, x)), zip(ind.fitness.weights, ind.fitness.values))) + ")"
-                f.write("Fitness {} = {}\n".format(sum(ind.fitness.wvalues), fitt_comp))
-                f.write(str(ind))
-                f.write("===\n\n")
-            f.close()
-            write_to_zip_and_remove(temp, path)
+    def save_readable_population(pop, num_rooms, num_periods, folder, file):
+        path = os.path.join(folder, file)
+        temp = temp_file(file)
+        f = open(temp, 'w')
+        pop = sorted(pop, key=lambda x: sum(x.fitness.wvalues), reverse=True)
+        for i, ind in enumerate(pop):
+            fitt_comp = "(" + ") + (".join(
+                map(lambda x: "*".join(map(str, x)), zip(ind.fitness.weights, ind.fitness.values))) + ")"
+            f.write("Fitness {} = {}\n".format(sum(ind.fitness.wvalues), fitt_comp))
+            f.write(str(ind))
+            f.write("===\n\n")
+        f.close()
+        write_to_zip_and_remove(temp, path)
 
-        def plot_progress(logbook, parameters, folder, file):
-            plt.clf()
-            path = os.path.join(folder, file)
-            temp = temp_file(file)
-            parameter_str = ", ".join([str(k) + '$=' + str(parameters[k]) + '$' for k in sorted(parameters)])
-            plot_ea_progress(logbook, parameter_str)
-            plt.savefig(temp)
-            write_to_zip_and_remove(temp, path)
+    def plot_progress(logbook, parameters, folder, file):
+        plt.clf()
+        path = os.path.join(folder, file)
+        temp = temp_file(file)
+        parameter_str = ", ".join([str(k) + '$=' + str(parameters[k]) + '$' for k in sorted(parameters)])
+        plot_ea_progress(logbook, parameter_str)
+        plt.savefig(temp)
+        write_to_zip_and_remove(temp, path)
 
-        # Save
+    # Save
+    if not hasattr(ea, 'last_save') or ea.last_save + 1000. < time.time() or always_save:
+        ea.last_save = time.time()
+        print("Saving {}".format(ea.name))
         log_dir = "logs"
         create_directory(log_dir)
         name = str(ea.name).replace(" ", "_")
@@ -113,7 +123,11 @@ def save_date(ea):
         save_str(ea.logbook, "logbook", "show.txt")
         save_str(str(ea), "logbook", "info.json")
         save_readable_population(ea.pop, ea.num_rooms, ea.num_periods, "pop", "show.txt")
-        plot_progress(ea.logbook, ea.jsonify()["ea"], "pop", "plot.png")
+        if ea.logbook is not None:
+            plot_progress(ea.logbook, ea.jsonify()["ea"], "pop", "plot.png")
+        zip_file.close()
+        print("Done saving {}".format(ea.name))
+
 
 if __name__ == "__main__":
     main(*sys.argv[1:])
