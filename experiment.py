8e9c25512db5e57192f84b5c036e27ec8742fec0
Frank Dorssers <frank.dorssers@gmail.com>
2015-06-15 16:02:46 +0200

    Add command line parser

--- experiment.py
+++ experiment.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 import threading
 import matplotlib
+import argparse
 
 matplotlib.use("Agg")
 
@@ -22,23 +23,9 @@ __author__ = 'pieter'
 q = Queue()
 
 
-def main(individuals=10, generations=3, crossover_pb=0.5, mutation_pb=0.2, dataset=1, experiment_name='NA', init_ea_file=None):
-    # Parse possible commandline arguments
+def main(individuals=[10], generations=[3], crossover_pb=[0.5], mutation_pb=[0.2], dataset=[1], experiment_name='NA', init_ea_file=None):
 
-    def parse_list_or_number(param, type):
-        try:
-            return [type(param)]
-        except ValueError:
-            return list(map(type, param.split(",")))
-
-    # init_ea_file = 'logs/ea_2015_06_12_17_45_00_7696.zip'
-
-    individuals = parse_list_or_number(individuals, int)
-    generations = parse_list_or_number(generations, int)
-    crossover_pb = parse_list_or_number(crossover_pb, float)
-    mutation_pb = parse_list_or_number(mutation_pb, float)
-    datasets = parse_list_or_number(dataset, int)
-    datasets = list(["data/exam_comp_set{}.exam".format(dataset) for dataset in datasets])
+    datasets = list(["data/exam_comp_set{}.exam".format(ds) for ds in dataset])
 
     eas = []
 
@@ -165,4 +152,30 @@ def save_data(ea, pop, logbook):
 
 
 if __name__ == "__main__":
-    main(*sys.argv[1:])
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument('-p', '--population', type=int, nargs='+', help='size of population', required=False)
+    argument_parser.add_argument('-g', '--generation', type=int, nargs='+', help='number of generations', required=False)
+    argument_parser.add_argument('-cpb', '--crossover', type=float, nargs='+', help='crossover probability', required=False)
+    argument_parser.add_argument('-mpb', '--mutation', type=float, nargs='+', help='mutation probability', required=False)
+    argument_parser.add_argument('-s', '--setnumber', type=int, nargs='+', help='number of the dataset', required=False)
+    argument_parser.add_argument('-b', '--batchname', help='name of the batch', required=False)
+    argument_parser.add_argument('-i', '--initfile', help='file from previous run', required=False)
+    args = argument_parser.parse_args()
+
+    params = dict()
+    if args.population:
+        params['individuals'] = args.population
+    if args.generation:
+        params['generations'] = args.generation
+    if args.crossover:
+        params['crossover_pb'] = args.crossover
+    if args.mutation:
+        params['mutation_pb'] = args.mutation
+    if args.setnumber:
+        params['dataset'] = args.setnumber
+    if args.batchname:
+        params['experiment_name'] = args.batchname
+    if args.initfile:
+        params['init_ea_file'] = args.initfile
+
+    main(**params)
